767f682d9dff14a536e2ad925cb8f6a74d2881de
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-06 17:58:34 +0530

    mapfunction

--- src/component/Strength/Strength.js
+++ src/component/Strength/Strength.js
@@ -1,26 +1,32 @@
 import './Strength.css'
 
 function Strength(){
+  const strengtharr =[
+    { "number":6,"Titel":"Ug program"},
+    { "number":2000,"Titel":"Students"},
+    { "number":135,"Titel":"Teachers"},
+    { "number":186,"Titel":"Placement"}
+  ];
+
+
+
     return(
         <div className='ourstr'>
           <p className='strength' >Our Strength</p>
           <div className='str1'>
-            <div className='str2'>
-              <p>6 </p> 
-              <p>Ug program</p>
+            <div className='strength1' >
+              {strengtharr.map((data, index) => (
+                <div className='str2'>
+                <h2 key={index}>
+                {data.number}
+                <br/>
+                {data.Titel}
+                <br/>
+                </h2>
+                </div>
+              ))}
             </div >
-            <div className='str2'>
-              <p>2000 </p> 
-              <p>Students</p>
-            </div>
-            <div className='str2'>
-              <p>135 </p> 
-              <p>Teacher</p>
-            </div>
-            <div className='str2'>
-              <p>186+ </p> 
-              <p>placement</p>
-            </div>
+            
           </div>
         </div>
     );
